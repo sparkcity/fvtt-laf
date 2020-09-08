49e2d40efe3697f69fa10ee2ee32177f397c2ecf
Parker <23155686+sparkcity@users.noreply.github.com>
2020-09-08 17:51:51 -0400

    initial commit - actor sheet nonfunctional

---
+++ module/actor-sheet.js
@@ -0,0 +1,72 @@
+import { ATTRIBUTE_TYPES } from "./constants.js";
+
+/**
+ * Extend the basic ActorSheet with some very simple modifications
+ * @extends {ActorSheet}
+ */
+export class SimpleActorSheet extends ActorSheet {
+
+  /** @override */
+	static get defaultOptions() {
+	  return mergeObject(super.defaultOptions, {
+  	  classes: ["laf", "sheet", "actor"],
+  	  template: "systems/laf/templates/actor-sheet.html",
+      width: 600,
+      height: 600,
+      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
+      dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  getData() {
+    const data = super.getData();
+    data.dtypes = ATTRIBUTE_TYPES;
+    for ( let attr of Object.values(data.data.attributes) ) {
+      attr.isCheckbox = attr.dtype === "Boolean";
+      attr.isResource = attr.dtype === "Resource";
+    }
+    data.shorthand = !!game.settings.get("laf", "macroShorthand");
+    return data;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+	activateListeners(html) {
+    super.activateListeners(html);
+
+    // Handle rollable attributes.
+    html.find('.items .rollable').click(ev => {
+      let button = $(ev.currentTarget);
+      let r = new Roll(button.data('roll'), this.actor.getRollData());
+      const li = button.parents(".item");
+      const item = this.actor.getOwnedItem(li.data("itemId"));
+      r.roll().toMessage({
+        user: game.user._id,
+        speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+        flavor: `<h2>${item.name}</h2><h3>${button.text()}</h3>`
+      });
+    });
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Update Inventory Item
+    html.find('.item-edit').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      const item = this.actor.getOwnedItem(li.data("itemId"));
+      item.sheet.render(true);
+    });
+
+    // Delete Inventory Item
+    html.find('.item-delete').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      this.actor.deleteOwnedItem(li.data("itemId"));
+      li.slideUp(200, () => this.render(false));
+    });
+
+  }
+}
